9746223dbb24d52315dc176cb2c587682f25dbbb
ViktorLohutenkov <113467775+ViktorLohutenkov@users.noreply.github.com>
2023-03-12 14:46:36 +0200

    Delete CurrencyRateNbuDto.java

--- group-6-telegram-bot/src/main/java/edu/goit/serviceNbu/CurrencyRateNbuDto.java
+++
@@ -1,9 +0,0 @@
-package edu.goit.serviceNbu;
-
-import lombok.Data;
-import java.math.BigDecimal;
-@Data
-public class CurrencyRateNBUDto {
-    private BigDecimal rate;
-    private Currency cc;
-}
