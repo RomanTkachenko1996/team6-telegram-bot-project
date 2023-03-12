d42289423610a716d0457a049ddec89376817b0a
ViktorLohutenkov <viktor.lohutenkov@gmail.com>
2023-03-12 14:37:41 +0200

    serviceNbu

--- group-6-telegram-bot/src/main/java/edu/goit/serviceNbu/CurrencyNBUService.java
+++ group-6-telegram-bot/src/main/java/edu/goit/serviceNbu/CurrencyNBUService.java
@@ -7,23 +7,15 @@ import org.jsoup.Jsoup;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
-
-import static CurrencyServiceNBU.Currency.EUR;
-import static CurrencyServiceNBU.Currency.USD;
 public class CurrencyNBUService implements CurrencyRetrievalService {
     private static final String URL = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
-    private static Map<Integer, Currency> codeCurr = Map.of(
-            840, USD,
-            978, EUR
-    );
     @Override
     public List<CurrencyRateDto> getCurrencyRates() throws IOException {
         String response = Jsoup.connect(URL).ignoreContentType(true).get().body().text();
         List<CurrencyRateNBUDto> responseDtos = convertResponseToList(response);
         return responseDtos.stream()
-                .filter(item -> codeCurr.containsKey(item.getR030()))
+                .filter(item -> item.getCc() != null)
                 .map(item -> new CurrencyRateDto(item.getCc(), null, item.getRate()))
                 .collect(Collectors.toList());
     }
