80e53d165d1aa1431e78425bb3a6860255eb8e03
RomanTkachenko1996 <110996538+RomanTkachenko1996@users.noreply.github.com>
2023-03-12 14:18:01 +0200

    deleted unnecessary files

--- group-6-telegram-bot/src/main/java/edu/goit/AppLauncher.java
+++
@@ -1,9 +0,0 @@
-package edu.goit;
-
-import edu.goit.telegram.BotService;
-
-public class AppLauncher {
-    public static void main(String[] args) {
-        BotService bs = new BotService();
-    }
-}
--- group-6-telegram-bot/src/main/java/edu/goit/ccy/CCY.java
+++
@@ -1,8 +0,0 @@
-package edu.goit.ccy;
-
-
-public enum CCY {
-        USD,
-        EUR
-
-}
